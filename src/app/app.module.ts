87fdc943df5689d621734521cb325d34442cf64f
xin.shen <969187979@qq.com>
2020-03-10 16:41:03 +0800

    submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from "./product-list/product-list.component"
-import { TopBarComponent } from "./top-bar/top-bar.component"
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component"
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    TopBarComponent
+    TopBarComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -14,4 +14,9 @@ export class ProductListComponent {
     window.alert("The product has been shared!")
   }
 
+  onNotify() {
+    console.log("hello world::: ")
+    window.alert("You will be notified when the product goes on Sale")
+  }
+
 }
